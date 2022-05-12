3b4389a3293269eccaa40429b9f8707254f1ca4e
vilkas99 <victorium99@hotmail.com>
2022-05-12 13:00:58 -0500

    Forms done

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import requests
-from flask import Flask, render_template, session, redirect, url_for, flash
+from flask import Flask, render_template, session, redirect, url_for, flash, request
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired
 from bs4 import BeautifulSoup
+from wtforms.validators import DataRequired, Length
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
@@ -19,6 +20,23 @@ class NameForm(FlaskForm):
     name = StringField('Introduce here your Name', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
+
+def getNamesDescriptionBreeds(breeds): 
+    finalNames = []
+
+    for breed in breeds: 
+        finalNames.append(breed["name"])
+
+    return finalNames
+
+class KittyForm(FlaskForm): 
+    breeds = requests.get("https://api.thecatapi.com/v1/breeds").json()
+    breedNames = getNamesDescriptionBreeds(breeds)
+
+    nameKitty = StringField('Introduce el nombre de tu michi/gatito: ', validators=[DataRequired(), Length(max=64)])
+    nameOfOwner = StringField('Introduce tu nombre: ', validators=[DataRequired(), Length(max=64)])
+    breed = SelectField('raza', choices=breedNames)
+    submit = SubmitField('Submit')
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -44,21 +62,17 @@ def status():
     return render_template('status.html', name=session.get('name'))
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
-
-    
-
-@app.route('/Episodes/Titans')
-def episodes():
-    URL = "https://en.wikipedia.org/wiki/Titans_(2018_TV_series)"
-    page = requests.get(URL)
-    soup = BeautifulSoup(page.content, "html.parser")
-    rows = soup.find_all("tr", class_="vevent")
-    x=0
-    episodes=[]
-    for row in rows:
-        x+=1
-        episodes.append(str(x)+ " " + (row.find("td", class_="summary")).text.strip())
-    return("<br>".join(episodes))
+    form = KittyForm()
+    if(request.method == "GET"):
+        return render_template('index.html', form = form)
+    else: 
+        if form.validate_on_submit(): 
+            kittyName = form.nameKitty.data
+            useranme = form.nameOfOwner.data
+            breed = form.breed.data
+            
+            kittyData = requests.get(f'https://api.thecatapi.com/v1/images/search?breed_name={breed}').json()[0]["url"]
+            
+            return render_template('kitty.html', nameKitty=kittyName, nameOwner = useranme, url= kittyData)
